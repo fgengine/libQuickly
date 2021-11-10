53b0f56e50211f395bc6adc3e1a213b8c853701b
Alexander Trifonov <fgengine@gmail.com>
2021-11-10 11:48:33 +0300

    Refactoring

--- Sources/libQuicklyView/Layout/Default/QCompositionLayout+ZStack.swift
+++ Sources/libQuicklyView/Layout/Default/QCompositionLayout+ZStack.swift
@@ -9,13 +9,40 @@ public extension QCompositionLayout {
     
     struct ZStack {
         
+        public var size: Size
         public var entities: [IQCompositionLayoutEntity]
         
         public init(
             _ entities: [IQCompositionLayoutEntity]
         ) {
+            self.size = []
             self.entities = entities
         }
+        
+        public init(
+            size: Size = [],
+            entities: [IQCompositionLayoutEntity]
+        ) {
+            self.size = size
+            self.entities = entities
+        }
+        
+    }
+    
+}
+
+public extension QCompositionLayout.ZStack {
+    
+    struct Size : OptionSet {
+        
+        public var rawValue: UInt
+        
+        public init(rawValue: UInt) {
+            self.rawValue = rawValue
+        }
+        
+        public static let horizontal = Size(rawValue: 1 << 0)
+        public static let vertical = Size(rawValue: 1 << 1)
         
     }
     
@@ -34,20 +61,33 @@ extension QCompositionLayout.ZStack : IQCompositionLayoutEntity {
     @discardableResult
     public func layout(bounds: QRect) -> QSize {
         var maxSize = QSize.zero
+        if self.size.contains(.horizontal) == true {
+            maxSize.width = bounds.width
+        } else if self.size.contains(.vertical) == true {
+            maxSize.height = bounds.height
+        }
         for entity in self.entities {
-            let size = entity.layout(
+            let size = entity.size(available: bounds.size)
+            maxSize = maxSize.max(size)
+        }
+        for entity in self.entities {
+            entity.layout(
                 bounds: QRect(
                     topLeft: bounds.topLeft,
-                    size: entity.size(available: bounds.size)
+                    size: maxSize
                 )
             )
-            maxSize = maxSize.max(size)
         }
         return maxSize
     }
     
     public func size(available: QSize) -> QSize {
         var maxSize = QSize.zero
+        if self.size.contains(.horizontal) == true {
+            maxSize.width = available.width
+        } else if self.size.contains(.vertical) == true {
+            maxSize.height = available.height
+        }
         for entity in self.entities {
             let size = entity.size(available: available)
             maxSize = maxSize.max(size)
